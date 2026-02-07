5eedb1eacbd91925152ff8cfb6162130a067fe46
alecnossa665 <alecnossa@nessappllc.com>
2026-02-07 16:02:57 -0500

    build fix

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: [
     "@anthropic-ai/bedrock-sdk",
+    "@anthropic-ai/sdk",
     "@anthropic-ai/vertex-sdk",
     "@google/genai",
+    "@mistralai/mistralai",
+    "@opentelemetry/api",
+    "@opentelemetry/sdk-trace-base",
+    "@opentelemetry/sdk-trace-node",
     "openai",
-    "anthropic",
+    "promptlayer",
   ],
 };
 
